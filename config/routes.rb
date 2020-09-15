658a04e84497a9cc7ed7bd66fb2e58641d82f92e
Masatoshi <msts.oo0131@gmail.com>
2020-09-15 18:31:59 +0900

    room機能として管理機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+
   devise_for :users
+  root to: "rooms/index"
   get 'messages/index'
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create, :index]
 end
